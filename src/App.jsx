be1fba07451d57769c24311717f3e21dd868d079
alexikiphenix <alexikiphenix@gmail.com>
2023-10-18 13:44:23 +0200

    adding fx addHyphen

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,24 @@
+import { useState } from 'react';
 import Tweet from './components/Tweet';
 
 function App() {
+  let [tweets, setTweets] = useState(0);
+  let [userName, setUserName] = useState('marcus');
+
+  const addHyphen = () => 
+  {
+    setUserName('-'+userName+'-');
+  }
+
   return (
     <div>
       <h1>Hello world</h1>
+        <button onClick={addHyphen}>Ajouter - </button>
       <div className="tweet-container">
-        <Tweet name="Doe" content="Je suis inconnu"/>
-        <Tweet name="Jones" content="un méchant qui essaye d'être gentil" />
-        <Tweet name="Cormier" content="craint par tout le monde"/>
-        <Tweet name="McGregor" content="une star charismatique" />
+        <Tweet name={userName} content="Je suis inconnu" like={0} />
+        <Tweet name={userName} content="un méchant qui essaye d'être gentil" like={0} />
+        <Tweet name={userName} content="craint par tout le monde" like={0} />
+        <Tweet name={userName} content="une star charismatique" like={0} />
      
       </div>
     </div>
